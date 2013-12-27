dbd19934bcf8828f03bd4f9874e31105a9058575
Marco Agner <marco.agner@live.com>
2013-12-27 14:03:52 -0200

    primeiros testes

--- test/test.coffee
+++ test/test.coffee
@@ -4,16 +4,59 @@ steps = require '../libs/steps.js'
 
 describe 'exist', ->
 
-  it "should exist", (done) ->
+  it 'should exist', (done) ->
     expect(releasy).to.be.ok()
     done()
 
-  it "should exist", (done) ->
+  it 'should exist', (done) ->
     expect(steps).to.be.ok()
+    done()
+
+describe 'setup', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.setup).to.be.a('function')
+    done()
+
+describe 'run', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.run).to.be.a('function')
+    done()
+
+describe 'bump', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.bump).to.be.a('function')
     done()
 
 describe 'add', ->
 
-  it "should...", (done) ->
-    #expect something
+  it 'should be a function', (done) ->
+    expect(steps.add).to.be.a('function')
     done()
+
+describe 'commit', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.commit).to.be.a('function')
+    done()
+
+describe 'tag', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.tag).to.be.a('function')
+    done()
+
+describe 'push', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.push).to.be.a('function')
+    done()
+
+describe 'publish', ->
+
+  it 'should be a function', (done) ->
+    expect(steps.publish).to.be.a('function')
+    done()
+
--- test/test.js
+++ test/test.js
@@ -9,18 +9,68 @@
   steps = require('../libs/steps.js');
 
   describe('exist', function() {
-    it("should exist", function(done) {
+    it('should exist', function(done) {
       expect(releasy).to.be.ok();
       return done();
     });
-    return it("should exist", function(done) {
+    return it('should exist', function(done) {
       expect(steps).to.be.ok();
       return done();
     });
   });
 
+  describe('setup', function() {
+    return it('should be a function', function(done) {
+      expect(steps.setup).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('run', function() {
+    return it('should be a function', function(done) {
+      expect(steps.run).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('bump', function() {
+    return it('should be a function', function(done) {
+      expect(steps.bump).to.be.a('function');
+      return done();
+    });
+  });
+
   describe('add', function() {
-    return it("should...", function(done) {
+    return it('should be a function', function(done) {
+      expect(steps.add).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('commit', function() {
+    return it('should be a function', function(done) {
+      expect(steps.commit).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('tag', function() {
+    return it('should be a function', function(done) {
+      expect(steps.tag).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('push', function() {
+    return it('should be a function', function(done) {
+      expect(steps.push).to.be.a('function');
+      return done();
+    });
+  });
+
+  describe('publish', function() {
+    return it('should be a function', function(done) {
+      expect(steps.publish).to.be.a('function');
       return done();
     });
   });
