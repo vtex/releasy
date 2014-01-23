7d1b87e94996189cad3a76d74434bff53303e392
Rodrigo Dumont <rlsdumont@gmail.com>
2014-01-23 11:58:02 -0200

    Apply options from file to program execution fixes #5

--- bin/releasy.js
+++ bin/releasy.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 var program = require('commander'),
   Releasy = require('../libs/releasy'),
-  pkg = require('../package.json');
+  pkg = require('../package.json'),
+  steps = require('../libs/steps');
 
-var type = 'patch';
+var optionsFile = steps.getOptionsFile();
+
+var type = optionsFile.type || 'patch';
 var arguments = process.argv;
 if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(arguments[2]) != -1) {
   type = arguments[2];
@@ -15,10 +18,10 @@ if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(argument
 
 program.version(pkg.version)
   .usage('(major|minor|*patch*|prerelease) [options]')
-  .option('-f, --filename [path]', 'Your package manifest file [package.json]', 'package.json')
-  .option('-t, --tag-name [tag]', 'The prerelease tag in your version [beta]', 'beta')
-  .option('--npm-tag [tag]', 'Tag option for npm publish', '')
-  .option('-f, --folder [folder]', 'Folder option for npm publish', '')
+  .option('-f, --filename [path]', 'Your package manifest file [package.json]')
+  .option('-t, --tag-name [tag]', 'The prerelease tag in your version [beta]')
+  .option('--npm-tag [tag]', 'Tag option for npm publish')
+  .option('-f, --folder [folder]', 'Folder option for npm publish')
   .option('--stable', 'Mark this as a relese stable (no prerelease tag)')
   .option('--no-commit', 'Do not commit the version change')
   .option('--no-tag', 'Do not tag the version change')
@@ -28,6 +31,24 @@ program.version(pkg.version)
   .option('-s, --silent', 'Dont ask for confirmation')
   .option('-q, --quiet', "Don't write messages to console")
   .parse(arguments);
+
+var defaults = {
+  'filename': 'package.json',
+  'tag-name': 'beta',
+  'npm-tag': '',
+  'folder': '',
+  'stable': false,
+  'no-commit': false,
+  'no-tag': false,
+  'no-push': false,
+  'npm': false,
+  'dry-run': false,
+  'silent': false,
+  'quiet': false
+};
+
+for (var key in defaults)
+  program[key] = program[key] || optionsFile[key] || defaults[key];
 
 program.type = type;
 program.cli = true;
