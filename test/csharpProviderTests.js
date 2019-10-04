543605a018171285df51c35ff7ca82ad8e1923b5
Christian Kaisermann <christian@kaisermann.me>
2019-10-04 17:15:13 -0300

    Refactor tests to javascript

---
+++ test/csharpProviderTests.js
@@ -0,0 +1,232 @@
+const { rm, cat } = require('shelljs')
+const semver = require('semver')
+
+const CsharpVersionProvider = require('../lib/providers/csharp.js')
+
+const createFile = (filePath, contents) => contents.to(filePath)
+
+describe('CsharpVersionProvider', function() {
+  after(() => rm('-rf', 'test/AssemblyInfo.cs'))
+
+  describe('reading C# version', function() {
+    it('should return SemVer object from informational version', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyInformationalVersion("1.2.3-beta.4")]\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      const version = provider.readVersion()
+
+      // assert
+      version.format().should.equal('1.2.3-beta.4')
+      return done()
+    })
+
+    it('should fall back to file version', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      const version = provider.readVersion()
+
+      // assert
+      version.format().should.equal('1.2.3')
+      return done()
+    })
+
+    it('should fall back to assembly version', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      const version = provider.readVersion()
+
+      // assert
+      version.format().should.equal('1.2.3')
+      return done()
+    })
+
+    return it('should throw an error if a version cannot be found', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+// no version in here!\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act & assert
+      ;(() => provider.readVersion()).should.throw(
+        'Could not find version information in file test/AssemblyInfo.cs'
+      )
+      return done()
+    })
+  })
+
+  describe('writing C# version', function() {
+    it('should accept SemVer object', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyInformationalVersion("1.2.3-beta.4")]\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      provider.writeVersion(semver('2.3.4-alpha.5'))
+
+      // assert
+      cat('test/AssemblyInfo.cs').should.equal(
+        `\
+[assembly: AssemblyVersion("2.3.4")]
+[assembly: AssemblyFileVersion("2.3.4")]
+[assembly: AssemblyInformationalVersion("2.3.4-alpha.5")]\
+`
+      )
+      return done()
+    })
+
+    it('should accept string version', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyInformationalVersion("1.2.3-beta.4")]\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      provider.writeVersion('2.3.4-alpha.5')
+
+      // assert
+      cat('test/AssemblyInfo.cs').should.equal(
+        `\
+[assembly: AssemblyVersion("2.3.4")]
+[assembly: AssemblyFileVersion("2.3.4")]
+[assembly: AssemblyInformationalVersion("2.3.4-alpha.5")]\
+`
+      )
+      return done()
+    })
+
+    it('should append missing version attributes', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+[assembly: AssemblyVersion("1.2.3")]
+// nothing else\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      provider.writeVersion('2.3.4-alpha.5')
+
+      // assert
+      cat('test/AssemblyInfo.cs').should.equal(
+        `\
+[assembly: AssemblyVersion("2.3.4")]
+// nothing else
+[assembly: AssemblyFileVersion("2.3.4")]
+[assembly: AssemblyInformationalVersion("2.3.4-alpha.5")]
+\
+`
+      )
+      return done()
+    })
+
+    it('should not mess line endings', function(done) {
+      createFile(
+        'test/AssemblyInfo.cs',
+        '// [assembly: AssemblyVersion("x.x.x")]\r\n[assembly: AssemblyVersion("2.3.5")]\r\n[assembly: AssemblyFileVersion("2.3.5")]\r\n[assembly: AssemblyInformationalVersion("2.3.5-beta.3")]\r\n'
+      )
+
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      provider.writeVersion('2.3.4-alpha.5')
+
+      // assert
+      cat('test/AssemblyInfo.cs').should.equal(
+        '// [assembly: AssemblyVersion("2.3.4")]\r\n[assembly: AssemblyVersion("2.3.4")]\r\n[assembly: AssemblyFileVersion("2.3.4")]\r\n[assembly: AssemblyInformationalVersion("2.3.4-alpha.5")]\r\n'
+      )
+      return done()
+    })
+
+    return it('should append missing attributes without breaking extra line', function(done) {
+      // arrange
+      createFile(
+        'test/AssemblyInfo.cs',
+        `\
+// [assembly: AssemblyVersion("2.3.5")]
+[assembly: AssemblyVersion("2.3.5")]
+[assembly: AssemblyInformationalVersion("2.3.5-beta.3")]
+\
+`
+      )
+      const provider = new CsharpVersionProvider('test/AssemblyInfo.cs')
+
+      // act
+      provider.writeVersion('2.3.4-alpha.5')
+
+      // assert
+      cat('test/AssemblyInfo.cs').should.equal(
+        `\
+// [assembly: AssemblyVersion("2.3.4")]
+[assembly: AssemblyVersion("2.3.4")]
+[assembly: AssemblyInformationalVersion("2.3.4-alpha.5")]
+[assembly: AssemblyFileVersion("2.3.4")]
+\
+`
+      )
+      return done()
+    })
+  })
+
+  return describe('file support', function() {
+    it('should support .cs extensions', function() {
+      // act
+      const supports = CsharpVersionProvider.supports('somefile.cs')
+
+      // assert
+      return supports.should.be.true
+    })
+
+    return it('should not support any other extension', function() {
+      // act
+      const supports = CsharpVersionProvider.supports('arbitrary.extension')
+
+      // assert
+      return supports.should.be.false
+    })
+  })
+})
---
+++ test/test.js
@@ -0,0 +1,119 @@
+const should = require('should')
+const sinon = require('sinon')
+const Releasy = require('../lib/releasy.js')
+const steps = require('../lib/steps.js')
+
+let sandbox = null
+
+describe('steps', () =>
+  it('should exist', function(done) {
+    should(steps).be.ok
+    return done()
+  }))
+
+describe('releasy', function() {
+  beforeEach(() => (sandbox = sinon.sandbox.create()))
+
+  afterEach(() => sandbox.restore())
+
+  it('should exist', function(done) {
+    Releasy.should.be.ok
+    return done()
+  })
+
+  it('should call all steps in dry run', function() {
+    const options = {
+      dryRun: true,
+      filename: 'test/testpackage.json',
+      type: 'patch',
+      steps,
+      quiet: true,
+    }
+
+    sandbox.spy(steps, 'setup')
+    sandbox.spy(steps, 'release')
+    sandbox.spy(steps, 'preReleasy')
+    sandbox.spy(steps, 'postReleasy')
+    sandbox.spy(steps, 'scripts')
+    sandbox.spy(steps, 'spawn')
+
+    const releasy = Releasy(options)
+
+    releasy.then(function() {
+      steps.setup.called.should.be.true
+      steps.setup.firstCall.returnValue.newVersion.should.equal('1.0.1')
+      steps.release.called.should.be.true
+      steps.preReleasy.called.should.be.true
+      steps.postReleasy.called.should.be.true
+      steps.scripts.called.should.be.true
+      steps.spawn.args[0][0].should.equal('echo pre')
+      return steps.spawn.args[1][0].should.equal('echo post')
+    })
+    return releasy.fail
+  })
+
+  it('should call all steps in dry run using manifest', function() {
+    const options = {
+      dryRun: true,
+      filename: 'test/testversionnull.json',
+      type: 'patch',
+      steps,
+      quiet: true,
+    }
+
+    sandbox.spy(steps, 'setup')
+    sandbox.spy(steps, 'release')
+    sandbox.spy(steps, 'preReleasy')
+    sandbox.spy(steps, 'postReleasy')
+    sandbox.spy(steps, 'scripts')
+    sandbox.spy(steps, 'spawn')
+
+    const releasy = Releasy(options)
+
+    releasy.then(function() {
+      steps.setup.called.should.be.true
+      steps.setup.firstCall.returnValue.newVersion.should.equal('2.0.1')
+      steps.release.called.should.be.true
+      steps.preReleasy.called.should.be.true
+      steps.postReleasy.called.should.be.true
+      steps.scripts.called.should.be.true
+      steps.spawn.args[0][0].should.equal('echo pre')
+      return steps.spawn.args[1][0].should.equal('echo post')
+    })
+    return releasy.fail
+  })
+
+  return it("should default to manifest.json when file doesn't exist", function() {
+    const options = {
+      dryRun: true,
+      filename: 'test/package.json',
+      type: 'patch',
+      steps,
+      quiet: true,
+    }
+
+    sandbox.spy(steps, 'setup')
+    sandbox.spy(steps, 'release')
+    sandbox.spy(steps, 'preReleasy')
+    sandbox.spy(steps, 'postReleasy')
+    sandbox.spy(steps, 'scripts')
+    sandbox.spy(steps, 'spawn')
+
+    const releasy = Releasy(options)
+
+    releasy.then(function() {
+      steps.setup.called.should.be.true
+      steps.setup.firstCall.returnValue.newVersion.should.equal('2.0.1')
+      steps.release.called.should.be.true
+      steps.release.args[0][0].versionProvider.filePath.should.equal(
+        'test/manifest.json'
+      )
+      steps.preReleasy.called.should.be.true
+      steps.postReleasy.called.should.be.true
+      steps.scripts.called.should.be.true
+      steps.spawn.args[0][0].should.equal('echo pre')
+      return steps.spawn.args[1][0].should.equal('echo post')
+    })
+    return releasy.fail
+  })
+})
