fdc90fbc06ed571d455f9f22e9b55136e2e55291
Christian Kaisermann <christian@kaisermann.me>
2019-10-04 16:22:00 -0300

    Fix default values and "no-" flags

--- bin/releasy.js
+++ bin/releasy.js
@@ -1,15 +1,21 @@
 #!/usr/bin/env node
-var program = require('commander')
-var Releasy = require('../lib/releasy')
-var camelCase = require('camelcase')
-var pkg = require('../package.json')
-var steps = require('../lib/steps')
+const program = require('commander')
+const camelCase = require('camelCase')
 
-var optionsFile = steps.getOptionsFile()
+const releasy = require('../lib/releasy')
+const pkg = require('../package.json')
+const steps = require('../lib/steps')
 
-var type = optionsFile.type || 'patch'
-var args = process.argv
-if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(args[2]) !== -1) {
+const optionsFile = steps.getOptionsFile()
+
+let type = optionsFile.type || 'patch'
+let args = process.argv
+
+if (
+  ['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(
+    args[2]
+  ) !== -1
+) {
   type = args[2]
   if (type === 'pre') type = 'prerelease'
   console.log('Release:', type)
@@ -17,50 +23,41 @@ if (['major', 'minor', 'patch', 'promote', 'prerelease', 'pre'].indexOf(args[2])
   args = args.slice(0, 2).concat(args.slice(3))
 }
 
-program.version(pkg.version)
+program
+  .version(pkg.version)
   .usage('(major|minor|*patch*|prerelease) [options]')
-  .option('-f, --filename [path]', 'Your package manifest file [package.json]')
-  .option('-t, --tag-name [tag]', 'The prerelease tag in your version [beta]')
-  .option('--npm-tag [tag]', 'Tag option for npm publish')
-  .option('-f, --folder [folder]', 'Folder option for npm publish')
-  .option('--stable', 'Mark this as a relese stable (no prerelease tag)')
+  .option('-f, --filename [path]', 'Your package manifest file', 'package.json')
+  .option('-t, --tag-name [tag]', 'The prerelease tag in your version', 'beta')
+  .option('--npm-tag [tag]', 'Tag option for npm publish', '')
+  .option('-f, --folder [folder]', 'Folder option for npm publish', '')
+  .option('--stable', 'Mark this as a relese stable (no prerelease tag)', false)
   .option('--no-commit', 'Do not commit the version change')
   .option('--no-tag', 'Do not tag the version change')
   .option('--no-push', 'Do not push changes to remote')
-  .option('--notes', 'Publish notes to GitHub Release Notes. Personal Token is required to use this option')
-  .option('-n, --npm', 'Publish to npm')
-  .option('-d, --dry-run', 'Dont do anything, just show what would be done')
-  .option('-s, --silent', 'Dont ask for confirmation')
-  .option('-q, --quiet', "Don't write messages to console")
+  .option(
+    '--notes',
+    'Publish notes to GitHub Release Notes. Personal Token is required to use this option',
+    false
+  )
+  .option('-n, --npm', 'Publish to npm', false)
+  .option(
+    '-d, --dry-run',
+    'Dont do anything, just show what would be done',
+    false
+  )
+  .option('-s, --silent', 'Dont ask for confirmation', false)
+  .option('-q, --quiet', "Don't write messages to console", false)
   .parse(args)
 
-var defaults = {
-  'filename': 'package.json',
-  'tag-name': 'beta',
-  'npm-tag': '',
-  'folder': '',
-  'stable': false,
-  'no-commit': false,
-  'no-tag': false,
-  'no-push': false,
-  'notes': false,
-  'npm': false,
-  'dry-run': false,
-  'silent': false,
-  'quiet': false,
-}
-
-for (const key in defaults) {
-  const ccKey = camelCase(key)
-  program[ccKey] = program[ccKey] || optionsFile[key] || defaults[key]
+for (let [key, value] of Object.entries(optionsFile)) {
+  if (key.startsWith('no-')) {
+    key = key.slice(3, key.length)
+    value = !value
+  }
+  program[camelCase(key)] = value
 }
 
 program.type = type
 program.cli = true
 
-try {
-  return new Releasy(program)
-} catch (error) {
-  console.error(error.message.red)
-  exit(1)
-}
+releasy(program)
